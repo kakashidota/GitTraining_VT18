06f440bc91e2ac60f62a800a697b544c958376e1
kakashidota <robin.kamo@live.se>
2018-01-12 14:06:06 +0100

    Initial Commit

---
+++ GitTraining/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitTraining
+//
+//  Created by Robin kamo on 2018-01-12.
+//  Copyright © 2018 Robin kamo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
